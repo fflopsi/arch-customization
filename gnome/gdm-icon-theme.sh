4f29e7485ee6d8edb93c353b2d2ce572fb08235e
fflopsi <florian.l.frauenfelder@gmail.com>
2023-01-30 13:05:43 +0100

    Add gdm-icon-theme.sh to change the gdm icon theme to Papirus

---
+++ gnome/gdm-icon-theme.sh
@@ -0,0 +1,6 @@
+#!/bin/env zsh
+
+# Script to apply the "Papirus" icon theme to the gnome login screen gdm
+# Change "Papirus" to something else to apply another icon theme
+
+sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface icon-theme Papirus
