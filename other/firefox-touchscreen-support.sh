7562f36934e951fc07c3758e6f3723bf78b9d2c2
fflopsi <florian.l.frauenfelder@gmail.com>
2023-02-21 10:38:05 +0100

    Fix sudo echo call

--- other/firefox-touchscreen-support.sh
+++ other/firefox-touchscreen-support.sh
@@ -2,4 +2,4 @@
 
 # Add a necessary line to a file to enable full touchscreen support for Firefox
 
-sudo echo MOZ_USE_XINPUT2 DEFAULT=1 >> /etc/security/pam_env.conf
+sudo bash -c "echo MOZ_USE_XINPUT2 DEFAULT=1 >> /etc/security/pam_env.conf"
